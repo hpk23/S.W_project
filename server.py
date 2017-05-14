87ef88a354f50548321990a1e0867c6352ea46e8
hpk23 <hpk23@naver.com>
2017-05-15 00:32:19 +0900

    파일크기에 대한 프로토콜 변경1(#8)

--- server.py
+++ server.py
@@ -16,26 +16,7 @@ if __name__ == "__main__" :
     while True :
         connection = UdpSocket(PORT, BUFSIZE=BUFSIZE)
 
-        protocol, addr = connection.receive_message()
-
-        if protocol == '0' :
-            print "TCP Protocol\n"
-
-            connection = TcpSocket(PORT, SERVER=True, LISTEN_NUMBER=LISTEN_NUMBER, BUFSIZE=BUFSIZE)
-
-            client_sock, addr = connection.accept_sock.accept()
-            connection.setClient(client_sock, addr)
-            thread = ServerThread(connection)
-            thread.run()
-            del thread
-            continue
-
-        print "UDP Protocol\n"
-
-        # UDP SERVER
-
-        connection.send_message(addr, "3team server")
-        time.sleep(1)
+        message, addr = connection.receive_message()
 
         msg = ""
         count = 0
@@ -43,22 +24,43 @@ if __name__ == "__main__" :
             list_str = item['rank'] + '. ' + item['music'] + '\n'
             msg += list_str
             count += 1
-            if count == 10 : break
-        connection.send_message(addr, msg)  #음악리스트를 클라이언트에 전송
+            if count == 10: break
 
-        number, addr = connection.receive_message() # 클라이언트가 선택한 번호를 전송받음
+        connection.send_message(addr, msg)  # 음악리스트를 클라이언트에 전송
+
+        number, addr = connection.receive_message()  # 클라이언트가 선택한 번호를 전송받음
         number = int(number)
 
-        #connection.sock.settimeout(3)
-
         directory_path = "D:/2017_S.W/1st"
-        if number is 0 :
+        if number is 0:
             connection.send_directory(addr, directory_path)
 
-        else :
+        else:
             number = str(number)
             file_name = connection.collection.find({"rank": number})[0]["music"]
             connection.send_message(addr, file_name + ".mp3")
+
             file_name = directory_path + '/' + file_name + ".mp3"
-            connection.send_file(addr, file_name)
-        del connection
+
+            file_size = os.path.getsize(file_name)
+            connection.send_message(str(file_size))
+
+            if file_size > 1024 * 60 :
+
+                print "TCP Protocol\n"
+                connection = TcpSocket(PORT, SERVER=True, LISTEN_NUMBER=LISTEN_NUMBER, BUFSIZE=BUFSIZE)
+                client_sock, addr = connection.accept_sock.accept()
+                connection.setClient(client_sock, addr)
+                thread = ServerThread(connection)
+                thread.run()
+                del thread
+                continue
+
+            else :
+                connection.send_file(addr, file_name)
+                del connection
+
+
+
+
+
